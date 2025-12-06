cc7bf766fa0d09b794222f03c6d20e40d238f2d8
Ector55 <ettore.didonato01@gmail.com>
2025-12-06 13:44:11 +0100

    New Frontend pages

---
+++ FrontendLarge/src/basicPageScript.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+    const intervalTime = 5000; 
+
+    function nextSlide() {
+        const currentSlide = slides[currentIndex];
+        
+        const nextIndex = (currentIndex + 1) % slides.length;
+        const nextSlide = slides[nextIndex];
+
+        currentSlide.classList.remove('active');
+        currentSlide.classList.add('exit');
+
+        nextSlide.classList.add('active');
+        nextSlide.classList.remove('exit');
+
+        setTimeout(() => {
+            currentSlide.classList.remove('exit');
+        }, 600);
+
+        currentIndex = nextIndex;
+    }
+
+    setInterval(nextSlide, intervalTime);
+});
